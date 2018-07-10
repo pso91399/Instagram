f1c3ac3686af548592bdbfb325ca361e62d17c77
Patricia Ouyang <patriciaouyang@fb.com>
2018-07-10 14:05:19 -0700

    Finished sign up and persisting users

--- app/src/main/java/com/example/patriciaouyang/instagram/MainActivity.java
+++ app/src/main/java/com/example/patriciaouyang/instagram/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.EditText;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,35 +22,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        usernameInput = findViewById(R.id.userName);
-        passwordInput = findViewById(R.id.password);
-        loginBtn = findViewById(R.id.btnLogin);
-        signUpBtn = findViewById(R.id.btnSignUp);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            // do stuff with the user
+            final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // show the signup or login screen
+            setContentView(R.layout.activity_main);
 
+            usernameInput = findViewById(R.id.userName);
+            passwordInput = findViewById(R.id.password);
+            loginBtn = findViewById(R.id.btnLogin);
+            signUpBtn = findViewById(R.id.btnSignUp);
 
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final String username = usernameInput.getText().toString();
+                    final String password = passwordInput.getText().toString();
 
+                    login(username, password);
+                }
+            });
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String username = usernameInput.getText().toString();
-                final String password = passwordInput.getText().toString();
-
-                login(username, password);
-            }
-        });
-
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String username = usernameInput.getText().toString();
-                final String password = passwordInput.getText().toString();
-
-                signup(username, password, "email");
-            }
-        });
+            signUpBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    signup();
+                }
+            });
+        }
     }
 
     private void login(String username, String password) {
@@ -72,33 +75,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void signup(String username, String password, String email) {
-        // Create the ParseUser
-        ParseUser user = new ParseUser();
-        // Set core properties
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setEmail(email);
-        // Set custom properties
-        //user.put("phone", "650-253-0000");
-        // Invoke signUpInBackground
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                    Log.d("SignupActivity", "Sign up successful!");
-
-                    final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                    Log.e("SignupActivity", "Sign up failure.");
-                    e.printStackTrace();
-                }
-            }
-        });
+    private void signup() {
+        Intent i = new Intent (MainActivity.this, SignUpActivity.class);
+        startActivity(i);
+        finish();
     }
 }
 
---
+++ app/src/main/java/com/example/patriciaouyang/instagram/SignUpActivity.java
@@ -0,0 +1,75 @@
+package com.example.patriciaouyang.instagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private EditText fullnameInput;
+    private EditText emailInput;
+    private Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        usernameInput = findViewById(R.id.etUsername);
+        passwordInput = findViewById(R.id.etPassword);
+        fullnameInput = findViewById(R.id.etFullName);
+        emailInput = findViewById(R.id.etEmail);
+        signUpBtn = findViewById(R.id.btnSignUp);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup(usernameInput.getText().toString(), passwordInput.getText().toString(), emailInput.getText().toString(), fullnameInput.getText().toString());
+            }
+        });
+
+    }
+
+    private void signup(String username, String password, String email, String fullname) {
+
+
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        //user.setHandle(username);
+        //user.setfullName(fullname);
+        // Set custom properties
+        //user.put("phone", "650-253-0000");
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    Log.d("SignupActivity", "Sign up successful!");
+
+                    final Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Log.e("SignupActivity", "Sign up failure.");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
