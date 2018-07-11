a9de437e3f4073c1db7a73c43b2e72aea7d96c24
Patricia Ouyang <patriciaouyang@fb.com>
2018-07-11 11:44:44 -0700

    Finished swipe refresh

--- app/src/main/java/com/example/patriciaouyang/instagram/FeedActivity.java
+++ app/src/main/java/com/example/patriciaouyang/instagram/FeedActivity.java
@@ -1,6 +1,7 @@
 package com.example.patriciaouyang.instagram;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,6 +21,8 @@ public class FeedActivity extends AppCompatActivity {
     RecyclerView rvPosts;
     public static final int REQUEST_CODE = 20;
 
+    private SwipeRefreshLayout swipeContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +35,59 @@ public class FeedActivity extends AppCompatActivity {
         rvPosts.setLayoutManager(new LinearLayoutManager(this));
         rvPosts.setAdapter(postAdapter);
 
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync(0);
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
         loadTopPosts();
     }
+
+    public void fetchTimelineAsync(int page) {
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+        postsQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e ==  null) {
+                    postAdapter.clear();
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("FeedActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername()
+                        );
+                        Post post = new Post();
+                        post.setDescription(objects.get(i).getDescription());
+                        post.setUser(objects.get(i).getUser());
+                        post.setImage(objects.get(i).getImage());
+                        posts.add(post);
+                        postAdapter.notifyItemInserted(posts.size() - 1);
+                    }
+                    postAdapter.addAll(posts);
+                    // Now we call setRefreshing(false) to signal refresh has finished
+                    swipeContainer.setRefreshing(false);
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 
     private void loadTopPosts() {
         final Post.Query postsQuery = new Post.Query();
